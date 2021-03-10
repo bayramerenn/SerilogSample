3986edd17a560266aadb2e67a671dfae2575a6c3
BEren <bayram.eren@outlook.com.tr>
2021-03-10 23:19:22 +0300

    Add project files.

---
+++ SeriLogSample/Program.cs
@@ -0,0 +1,36 @@
+﻿using Serilog;
+using System;
+
+namespace SeriLogSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Hello word");
+
+            int a = 10, b = 0;
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+           }
+            catch (Exception ex)
+            {
+
+                Log.Error(ex, "Something went wrong");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
+        }
+    }
+}
